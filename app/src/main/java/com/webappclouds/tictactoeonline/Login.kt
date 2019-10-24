a17c2e3cb026e6aa2ba4e9f427b98f8394368496
Dan Boehmke <Daniel@webappclouds.com>
2019-10-24 16:42:34 -0400

    Save user to database

--- app/src/main/java/com/webappclouds/tictactoeonline/Login.kt
+++ app/src/main/java/com/webappclouds/tictactoeonline/Login.kt
@@ -1,17 +1,19 @@
 package com.webappclouds.tictactoeonline
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
 
     private var mAuth: FirebaseAuth? = null
+    private var database = FirebaseDatabase.getInstance()
+    private var myRef = database.reference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,10 +33,10 @@ class Login : AppCompatActivity() {
                 if (task.isSuccessful) {
                     Toast.makeText(applicationContext, "Successful login", Toast.LENGTH_LONG).show()
 
-//                    var currentUser =mAuth!!.currentUser
-//                    if(currentUser!=null) {
-//                        myRef.child("Users").child(SplitString(currentUser.email.toString())).child("Request").setValue(currentUser.uid)
-//                    }
+                    var currentUser =mAuth!!.currentUser
+                    if(currentUser!=null) {
+                        myRef.child("Users").child(SplitString(currentUser.email.toString())).child("Request").setValue(currentUser.uid)
+                    }
 
                     loadMain()
                 } else {
@@ -59,5 +61,10 @@ class Login : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+    }
+
+    fun  SplitString(str:String):String{
+        var split=str.split("@")
+        return split[0]
     }
 }

c2462a38a9425b90ba942d31b7ab751ac1e2b4cd
Dan Boehmke <Daniel@webappclouds.com>
2019-10-24 15:34:15 -0400

    Login Interface

---
+++ app/src/main/java/com/webappclouds/tictactoeonline/Login.kt
@@ -0,0 +1,16 @@
+package com.webappclouds.tictactoeonline
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun butLoginEvent(view:View) {
+        
+    }
+}
